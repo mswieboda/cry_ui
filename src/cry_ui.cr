0e8ef6ecac9cd91b557705e1971d45fba50a609e
Matt Swieboda <mswiebod@gmail.com>
2021-01-26 00:08:50 -0600

    adds libui, crystal Iu wrapper and example app

--- src/cry_ui.cr
+++ src/cry_ui.cr
@@ -1,6 +1,71 @@
-# TODO: Write documentation for `CryUI`
-module CryUI
-  VERSION = "0.1.0"
+require "iu"
 
-  # TODO: Put your code here
+alias GUI = Iu
+
+module CryUI
+  # defines global component storage
+  COMPONENTS = {} of Symbol => GUI::Component
+
+  class MyComponent < GUI::ReusableComponent
+    include GUI::Components
+
+    def initialize(@id : Int32)
+      @button = Button.new(text: "Click Me!")
+
+      @button.clicked.on do
+        UI.msg_box(COMPONENTS[:window].as(Window).window, "Good Job!", "You have clicked a button!")
+      end
+    end
+
+    def render : GUI::Component
+      Group.new(
+        title: "MyComponent",
+        margined: true
+      ).adopt(
+        VerticalBox.new(padded: true).adopt(
+          Label.new(text: "Hello, World ##{@id}!")
+        ).adopt(
+          @button
+        )
+      )
+    end
+  end
+
+  class Application < GUI::Application
+    include GUI::Components
+
+    def initialize_component
+      window = Window.new(
+        title: "CryUI",
+        width: 800,
+        height: 600,
+        menu_bar: false
+      )
+
+      window.margined = true
+
+      window.adopt(
+        HorizontalBox.new(padded: true).adopt(
+          MyComponent.new(1),
+          stretchy: true
+        )
+      )
+
+      COMPONENTS[:window] = window
+
+      window.closing.on do
+        exit(0)
+      end
+
+      window.show
+    end
+  end
 end
+
+app = CryUI::Application.new
+
+app.should_quit.on do
+  exit(0)
+end
+
+app.start
